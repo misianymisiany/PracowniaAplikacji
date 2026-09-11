e00115a8bb48b1929f316b2de8d44b557230f998
Kacper Rejdych <test@test.com>
2026-09-11 11:54:30 +0200

    zadania 6

--- zadania01/src/Main.java
+++ zadania01/src/Main.java
@@ -14,4 +14,37 @@ void main() {
 
     System.out.println("Mam na imię "+imie+"  "+"mam "+wiek+" lat  i będę pisać maturę za"+liczba+" roku");
 
+Scanner sc= new Scanner(System.in);
+
+
+    System.out.print("Podaj temperaturę w stopniach Celsjusza: ");
+    double temperatura = sc.nextDouble();
+    System.out.println(temperatura);
+    double fahrenheity = 1.8 * temperatura + 32.0;
+    System.out.println(temperatura + " stopni Celsjusza to " + fahrenheity + " stopni Fahrenheita.");
+    System.out.println("podaj bok 1");
+    double bok1 =sc.nextDouble();
+    System.out.println("podaj bok 2");
+    double bok2 =sc.nextDouble();
+    System.out.println("podaj bok 3");
+    double bok3 =sc.nextDouble();
+    double obw = bok1+bok2+bok3;
+    System.out.println("obwód trójkąta to: "+obw);
+
+
+    System.out.println("Podaj trzy słowa (oddzielone spacjami lub enterem):");
+    String slowo1 = sc.next();
+    String slowo2 = sc.next();
+    String slowo3 = sc.next();
+    System.out.println(slowo3 + ", " + slowo2 + ", " + slowo1);
 }
+
+
+
+
+
+///Napisz program, który wczyta od użytkownika trzy słowa i wypisze je w odwrotnej kolejności, niż podał je użytkownik, oddzielone przecinkami. Dla przykładu, gdy użytkownik poda:
+///
+/// Ala
+/// ma
+/// kota
